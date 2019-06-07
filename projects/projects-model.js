42918c998728e21b24012d1990d99f3b75294987
ken1286 <kindle1286@gmail.com>
2019-06-07 08:55:03 -0700

    projects-model added

---
+++ projects/projects-model.js
@@ -0,0 +1,39 @@
+const knex = require('knex');
+
+const knexConfig = require('../knexfile.js');
+
+const db = knex(knexConfig.development);
+
+module.exports = {
+  addProject,
+  addAction,
+  getProject
+};
+
+async function getProject(id) {
+  const project = await db('projects')
+    .where({ id })
+    .first()
+
+  const projectActions = await db('projects')
+    .join('actions', 'actions.project_id', 'projects.id')
+    .select('actions.id', 'actions.description', 'actions.notes', 'actions.completed')
+    .where({'actions.project_id': id})
+
+  const actions = await projectActions.map(action => action)
+  const result = {
+                  ...project,
+                  actions
+                };
+  return result;
+}
+
+function addProject(project) {
+  return db('projects')
+    .insert(project);
+}
+
+function addAction(action) {
+  return db('actions')
+    .insert(action);
+}
---
+++ projects/projects-router.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const Projects = require('./projects-model.js');
+
+router.post('/projects', (req, res) => {
+
+  Projects
+    .addProject(req.body)
+    .then(project => {
+      res.status(201).json({project})
+    })
+    .catch(err => {
+      res.status(500).json({err})
+    })
+})
+
+router.post('/actions', (req, res) => {
+
+  Projects
+    .addAction(req.body)
+    .then(action => {
+      res.status(201).json({action})
+    })
+    .catch(err => {
+      res.status(500).json({err})
+    })
+})
+
+router.get('/projects/:id', (req, res) => {
+  console.log(req.params.id)
+
+  Projects
+    .getProject(req.params.id)
+    .then(result => {
+      console.log(result);
+      res.status(200).json(result)
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+module.exports = router;
